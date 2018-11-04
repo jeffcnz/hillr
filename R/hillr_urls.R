7dac3a905b9311f56bced4133afa3e40dbdfd1a9
jeffcnz <jeffjcooke@hotmail.com>
2018-11-04 20:35:29 +1300

    Initial commit

---
+++ R/hillr_urls.R
@@ -0,0 +1,27 @@
+# http://data.hbrc.govt.nz/EnviroData/Emar.hts?Service=Hilltop&Request=GetData&Site=Ngaruroro River at Fernhill&Measurement=Total Nitrogen&From=1/6/2014"
+
+#' Create a url to request a site list from a Hilltop Server.
+#'
+#' Takes a valid Hilltop server endpoint and returns a url that requests the
+#' site list from the endpoint.
+#' @param endpoint A valid hilltop endpoint.
+#' @return A Hilltop url request.
+#' @export
+buildSiteListUrl <- function(endpoint) {
+  # check whether a '?' is the last charachter of the endpoint, if not add one.
+  endpoint <- fixEndpoint(endpoint)
+  # check that the endpoint is valid.
+  if(!is.validEndpoint(endpoint)) {
+    stop("Invalid endpoint provided.")
+  }
+  # set the service as 'Hilltop'
+  service <- "Hilltop"
+  # set the request as 'SiteList'
+  request <- "SiteList"
+  # build the url
+  hillUrl <- base::paste0(endpoint,
+                    "Service=", service,
+                    "&Request=", request)
+  # return the url
+  return(hillUrl)
+}
