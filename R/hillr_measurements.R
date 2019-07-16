9498b9911999868f05b4e01414ecddc0daea804f
jeffcnz <jeffjcooke@hotmail.com>
2019-07-17 08:25:47 +1200

    Data source to DF bug fix

--- R/hillr_measurements.R
+++ R/hillr_measurements.R
@@ -87,7 +87,7 @@ hilltopDataSourceToDF<-function(xmldata) {
   } else {
     #Extract the measurement name from the xml and return this in a dataframe
     #that can be associated with the measuremnts.
-    itemName <- dataxml[["string(//DataSource/@Name)"]]
+    itemName <- xmldata[["string(//DataSource/@Name)"]]
     base::data.frame(ItemName = itemName, stringsAsFactors = FALSE)
   }
   #return the metadata
